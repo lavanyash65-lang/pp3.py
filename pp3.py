64b31b769425491138d767c6fe3ca022e743c7b7
lavanyash65-lang <lavanyash65@gmail.com>
2025-10-06 15:31:33 +0530

    Update pp3.py

--- pp3.py
+++ pp3.py
@@ -4,7 +4,7 @@ principal = float(input("Enter the principal amount: "))
 rate = float(input("Enter the rate of interest (% per annum): "))
 time = float(input("Enter the time (in years): "))
 
-simple_interest = (principal * rate * time) / 100
+simple_interest = (principal * rate * time) / 100 
 
 print(f"Simple Interest: {simple_interest:.2f}")
 
